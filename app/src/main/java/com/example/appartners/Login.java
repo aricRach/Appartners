ebe06c59eb0aa257aaf5a2c080110715594996af
aricRach <aricrachmany@gmail.com>
2019-11-19 20:22:14 +0200

    authentication succeeded with email and password for the first time

--- app/src/main/java/com/example/appartners/Login.java
+++ app/src/main/java/com/example/appartners/Login.java
@@ -6,14 +6,18 @@ import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -21,67 +25,89 @@ import java.util.Map;
 
 public class Login extends AppCompatActivity {
 
-    private static final String TAG="Login";
-
-    private static final String Key_Username="username";
-    private static final String Key_password="password";
 
     private EditText editUserName;
     private EditText editPassword;
 
-    private FirebaseFirestore db =FirebaseFirestore.getInstance();
-
+    private FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth=FirebaseAuth.getInstance();
+
         CardView card_view = (CardView) findViewById(R.id.LoginCard); // creating a CardView and assigning a value.
 
         editUserName= (EditText)findViewById(R.id.EmailText);
         editPassword= (EditText)findViewById(R.id.passText);
 
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+
+                if (firebaseAuth.getCurrentUser()!=null){
+
+                    //goToReg(); //if i allow this it will take me to registration page but cant go back to login because
+                    // i already logged in ! need to fix it!! because it on "onCreate" function
+                }
+            }
+        };
+
         card_view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            //    Toast.makeText(Login.this, "HI your mail is: "+text_name.getText().toString(), Toast.LENGTH_SHORT).show();
-                String userName=editUserName.getText().toString();
-                String pass=editPassword.getText().toString();
 
-                Map<String,Object> note = new HashMap<>();
-                note.put(Key_Username,userName);
-                note.put(Key_password,pass);
-
-                db.collection("notebook").document("my first note").set(note)
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void aVoid) {
-
-                                Toast.makeText(Login.this, "success", Toast.LENGTH_SHORT).show();
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-
-                                Toast.makeText(Login.this, "Fail", Toast.LENGTH_SHORT).show();
-                            }
-                        });
+                tryToLogin();
 
             }
         });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    public void tryToLogin(){
+
+        String email=editUserName.getText().toString();
+        String password=editPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email)|| TextUtils.isEmpty(password)){
+
+            Toast.makeText(Login.this, "empty fields", Toast.LENGTH_SHORT).show();
+
+        }else{
+
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if (!task.isSuccessful()){
+
+                        Toast.makeText(Login.this, "Login not successed", Toast.LENGTH_SHORT).show();
+                    }else{
+
+                        Toast.makeText(Login.this, "You Logged in", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            });
+
+        }
+
+    }
+
     public void goToReg(View view){
 
-
         Intent intent=new Intent(this,register.class);
-
         startActivity(intent);
         Toast.makeText(this, "Clicked on Button", Toast.LENGTH_LONG).show();
-
-
 
     }
 
--- app/src/main/java/com/example/appartners/register.java
+++ app/src/main/java/com/example/appartners/register.java
@@ -2,13 +2,68 @@ package com.example.appartners;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class register extends AppCompatActivity {
+
+    //private static final String TAG="Login";
+
+    // private static final String Key_Username="username";
+    //  private static final String Key_password="password";
+
+
+    // private FirebaseFirestore db =FirebaseFirestore.getInstance(); important for registration page
+
+   // private EditText editUserName;
+    //private EditText editPassword;
+
+
+    // the following code need to be in some button listener after fill the registration fields
+
+    // IMPORTANT FOR REGISTER PAGE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    //    Toast.makeText(Login.this, "HI your mail is: "+text_name.getText().toString(), Toast.LENGTH_SHORT).show();
+//        String userName=editUserName.getText().toString();
+//        String pass=editPassword.getText().toString();
+//
+//        Map<String,Object> note = new HashMap<>();
+//        note.put(Key_Username,userName);
+//        note.put(Key_password,pass);
+//
+//        db.collection("notebook").document("my first note").set(note)
+//                .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                    @Override
+//                    public void onSuccess(Void aVoid) {
+//
+//                        Toast.makeText(Login.this, "success", Toast.LENGTH_SHORT).show();
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//
+//                        Toast.makeText(Login.this, "Fail", Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+
+    public void backToLogin(View view){
+
+
+        Intent intent=new Intent(this,Login.class);
+
+        startActivity(intent);
+        Toast.makeText(this, "back to login", Toast.LENGTH_LONG).show();
+
     }
 }
